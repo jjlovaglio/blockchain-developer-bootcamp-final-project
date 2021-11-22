d8adcff09f4ad8de73bb6912c35cff8a4b4783a7
jjlovaglio <jjlovaglio@gmail.com>
2021-11-22 11:47:16 -0300

    feat: first test created

--- contracts/TokenVines.sol
+++ contracts/TokenVines.sol
@@ -10,17 +10,23 @@ contract TokenVines is Ownable {
     uint256 public vineCount;
 
     // Event: 'State' with value 'ForSale'
+    enum Sale {
+        NotForSale,
+        ForSale
+    }
+
     enum State {
-        Harvested,
-        Unharvested
+        Unharvested,
+        Harvested
     }
 
     struct Vine {
         uint256 vineId;
         uint256 price;
+        Sale sale;
         State state;
         address payable seller;
-        address buyer;
+        address payable buyer;
     }
 
     // Define a public mapping 'items' that maps the SKU (a number) to an Item.
@@ -32,6 +38,14 @@ contract TokenVines is Ownable {
     // TODO: test all functionality in truffle develop
     // TODO: research how to handle events in front end
 
+    function putVineForSale(uint256 vineId) public {
+        // fetch vine
+        // change state
+
+        Vine storage v = vines[vineId];
+        v.sale = Sale.ForSale;
+    }
+
     function createVineyard(uint256 _count, uint256 _price) public onlyOwner {
         vineCount = _count;
         for (uint256 i = 0; i < _count; i++) {
@@ -39,9 +53,10 @@ contract TokenVines is Ownable {
             vines[i] = Vine({
                 vineId: i,
                 price: _price,
+                sale: Sale.ForSale,
                 state: State.Harvested,
                 seller: seller,
-                buyer: address(0)
+                buyer: payable(address(0))
             });
         }
     }
@@ -55,9 +70,10 @@ contract TokenVines is Ownable {
         vines[vineCount] = Vine({
             vineId: vineCount,
             price: _price,
+            sale: Sale.ForSale,
             state: State.Harvested,
             seller: payable(msg.sender),
-            buyer: address(0)
+            buyer: payable(address(0))
         });
     }
 
@@ -65,11 +81,16 @@ contract TokenVines is Ownable {
         address buyer = msg.sender;
         uint256 price = vines[_vineId].price;
 
+        // TODO: create forSale modifier
+
         // Update Buyer
-        vines[_vineId].buyer = buyer;
+        vines[_vineId].buyer = payable(buyer);
 
         // Update State
         vines[_vineId].state = State.Unharvested;
+
+        // Update Sale
+        vines[_vineId].sale = Sale.NotForSale;
 
         // Transfer money to seller
         vines[_vineId].seller.transfer(price);
@@ -81,22 +102,34 @@ contract TokenVines is Ownable {
         returns (
             uint256 vineId,
             uint256 price,
+            string memory saleIs,
             string memory stateIs,
             address seller,
             address buyer
         )
     {
         uint256 state;
+        uint256 sale;
         vineId = vines[_vineId].vineId;
         price = vines[_vineId].price;
+        sale = uint256(vines[_vineId].sale);
+
+        if (sale == 0) {
+            saleIs = "NotForSale";
+        }
+
+        if (sale == 1) {
+            saleIs = "ForSale";
+        }
+
         state = uint256(vines[_vineId].state);
 
         if (state == 0) {
-            stateIs = "Harvested";
+            stateIs = "Unharvested";
         }
 
         if (state == 1) {
-            stateIs = "Unharvested";
+            stateIs = "Harvested";
         }
 
         seller = vines[_vineId].seller;
--- test/token_vines.js
+++ test/token_vines.js
@@ -7,60 +7,63 @@ contract("TokenVines", function (accounts) {
     return assert.isTrue(true);
   });
 
-  it("has an initial value of 0", async() => {
+  it("can create a vineyard with 10 vines, each @ 100 price", async() => {
     // get the contract that has been deployed
-    const tvInstance = await TokenVines.deployed();
-
+    const i = await TokenVines.deployed();
+    console.log("here acc: "+ accounts[0])
     // verify it has an initial value of 0
-    const storedData = await tvInstance.getStoredData.call();
-    assert.equal(storedData, 0, "initial state should be zero");
+     let v = await i.createVineyard(10,100);
+     let r = await i.fetchVine(1)
 
+
+    await assert.equal(r.vineId.toNumber(), 1, "vineId should be one");
+    await assert.equal(r.price.toNumber(),100, "price should be 100" );
   });
 
-  describe("Functionality", () => {
-    it("should store a new value 42", async () => {
-      // grab the contract we need
-      const tvInstance = await TokenVines.deployed();
+  // describe("Functionality", () => {
+  //   it("should store a new value 42", async () => {
+  //     // grab the contract we need
+  //     const tvInstance = await TokenVines.deployed();
 
-      // change the number
-      await tvInstance.setStoredData(42, {from: accounts[0] });
+  //     // change the number
+  //     await tvInstance.setStoredData(42, {from: accounts[0] });
 
-      // make sure the 2 numbers match
-      const storedData = await tvInstance.getStoredData.call();
-      assert.equal(storedData, 42, `42 was not stored!`)
+  //     // make sure the 2 numbers match
+  //     const storedData = await tvInstance.getStoredData.call();
+  //     assert.equal(storedData, 42, `42 was not stored!`)
 
 
-    })
+  //   })
 
-  })
+  // })
 
-  it("should not let someone else change the owner variable", async () => {
-    // will destructure accounts and give them variable names
-    const [ owner, badJoe ] = accounts;
+  // it("should not let someone else change the owner variable", async () => {
+  //   // will destructure accounts and give them variable names
+  //   const [ owner, badJoe ] = accounts;
 
-    // deploys a new instance, passing an argument (42 in this case)
-    const tvInstance = await TokenVines.new(42, {from: owner });
+  //   // deploys a new instance, passing an argument (42 in this case)
+  //   const tvInstance = await TokenVines.new(42, {from: owner });
 
-    try {
-      await tvInstance.setStoredData(22, {from: badJoe });
-    } catch (err) { }
+  //   try {
+  //     await tvInstance.setStoredData(22, {from: badJoe });
+  //   } catch (err) { }
 
-    const storedData = await tvInstance.getStoredData.call();
+  //   const storedData = await tvInstance.getStoredData.call();
 
-    assert.equal(storedData, 42, "storedData was not changed!");
+  //   assert.equal(storedData, 42, "storedData was not changed!");
       
     
 
 
-  });
+  // });
 
-  it("should display the balance of the owner's account", async () => {
+  // it("should display the balance of the owner's account", async () => {
 
-    const balance = await web3.eth.getBalance(accounts[0]);
-    console.log(`owner's balance (in gwei): ${balance}`);
+  //   const balance = await web3.eth.getBalance(accounts[0]);
+  //   console.log(`owner's balance (in gwei): ${balance}`);
 
 
-  });
+  // });
 
 
 });
