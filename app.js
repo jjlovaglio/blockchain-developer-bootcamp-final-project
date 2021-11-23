e5a202ed8efee6d3d5f63e34b456036f8141dad0
jjlovaglio <jjlovaglio@gmail.com>
2021-11-23 14:53:15 -0300

    ready for initial heroku deployment

---
+++ app.js
@@ -0,0 +1,15 @@
+// create an express app
+const express = require("express")
+const app = express()
+
+// use the express-static middleware
+app.use(express.static("front_end"))
+
+// define the first route on express
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
+
+// start the server listening for requests on express
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
